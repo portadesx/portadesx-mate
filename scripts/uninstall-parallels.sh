4df45debc056cfef86def62222a0876fc7dc6410
Alif <arfshl@protonmail.com>
2025-08-10 14:13:14 +0700

    Create uninstall-parallels.sh

---
+++ scripts/uninstall-parallels.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+proot-distro remove portadesx-mate
+
+rm $PREFIX/etc/proot-distro/portadesx-mate.sh
+rm /data/data/com.termux/files/usr/bin/portadesx-mate-cli
+rm /data/data/com.termux/files/usr/bin/portadesx-mate-gui
+rm /data/data/com.termux/files/usr/bin/portadesx-mate-help
