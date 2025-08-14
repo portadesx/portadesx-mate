fd112d6ba9b386427ec92f0c9eef41412d0c3648
Alif <arfshl@protonmail.com>
2025-08-14 20:22:17 +0700

    Update portadesx-mate.sh

--- scripts/portadesx-mate.sh
+++ scripts/portadesx-mate.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="PortadesX:MATE"
-TARBALL_URL['aarch64']="034aeaffd62b56883cabc19610b511e9d9be9234790cb7d45b79442b03e0aa9c"
-TARBALL_SHA256['aarch64']="https://github.com/portadesx/portadesx-mate/releases/download/24.04-202508100649/portadesx-mate-2404.tar.xz"
+TARBALL_URL['aarch64']="10b6cc6b62b5a5565dbc1a209972f5c39bb87f7e71f8f0ff846485259ee2a621"
+TARBALL_SHA256['aarch64']="https://github.com/portadesx/portadesx-mate/releases/download/noble-aarch64/portadesx-mate.tar.xz"
 distro_setup() {
         run_proot_cmd ln -s /storage/emulated/0/ /home/portadesx/Desktop/android_files
 }
